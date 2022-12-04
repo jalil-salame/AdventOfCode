54020603f790cced5e53549e6dd26778e83eb56a
Jalil David Salamé Messina <jalil.salame@tum.de>
2022-12-04 18:06:27 +0100

    feat(prepare-day): Using cargo-generate

--- prepare-day.sh
+++ prepare-day.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+fail() {
+	echo "$1" >&2
+	exit 1
+}
+
 today="$(date +'%-d')"
 this_year="$(date +'%Y')"
 day="${1:-$today}"
@@ -10,30 +15,25 @@ lang="${3:-rust}"
 day_dir="$year/day/$day_zp"
 
 mkdir -pv "$day_dir"
-if [ -f "$year/session" ]; then
-	session="$(cat "$year/session")"
-else
-	echo "Session cookie doesn't exist"
-	exit 1
-fi
-
-input="$day_dir/input"
-
-if [ -f "$input" ]; then
-	echo "$input already exists"
-	exit 0
-else
-	curl -b "$session" -o "$input" "https://adventofcode.com/$year/day/$day/input"
-fi
 
 case "$lang" in
 'rust')
-	cd "$day_dir" || exit 1
-	cargo init --name "aoc-$year-$day_zp"
-	cp ../../../templates/main.rs src/main.rs
+	prev="$PWD"
+	cd "$day_dir" || fail "failed to cd into '$day_dir'"
+	cargo generate --init --name "aoc-$year-$day_zp" gh:salameme/aoc-template
+	cd "$prev" || fail "Couldn't cd back to the root"
 	;;
 *)
-	echo "No template for languange '$lang'"
-	exit 1
+	fail "No template for languange '$lang'"
 	;;
 esac
+
+input="$day_dir/input"
+
+if [ -f "session" ]; then
+	session="$(cat "session")"
+else
+	fail "Couldn't get input: Session cookie doesn't exist"
+fi
+
+curl -b "$session" -o "$input" "https://adventofcode.com/$year/day/$day/input" || fail "Couldn't download input for AoC $day $year"
